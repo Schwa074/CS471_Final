8368c0c7b5ce7743bfa3a0cff0995f03d3539100
Steve Schwarzmann <schwa074@csusm.edu>
2025-04-07 21:22:47 -0700

    Extract decision tree logic into its own file

---
+++ DecisionTree/decisiontree.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import pickle
+from sklearn.model_selection import GridSearchCV
+import time
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+
+def run_decision_tree(data_feature_names, data_target_names, train_features, train_labels, validation_features, validation_labels, test_features, test_labels):
+    start_time = time.time()
+    # dt_path = './pickledData/decisionTreeModel.pickle'  # Path to the pickled decision tree model
+
+    # Load data from a pickle file or reading CSV file and pickle it if needed. Should only happen once
+    # try:
+    #     with open(dt_path, "rb") as file:
+    #         dt = pickle.load(file)
+    # except FileNotFoundError:
+    #     dt = DecisionTreeClassifier(random_state=42)
+    #     dt.fit(train_features, train_labels)
+    #     with open(dt_path, "wb") as file:
+    #         pickle.dump(dt, file)  # Save decision tree model to a pickle for future use.
+
+    # param_grid = {
+    #     'max_depth': np.arange(1, 11),  # Depth of the tree (1 to 10)
+    #     'min_samples_split': np.arange(2, 11)  # Minimum samples to split (2 to 10)
+    # }
+
+    # To be more efficient, took best param to avoid doing every possible param
+    # See DecisionTreeBestParamOutput for inital run
+    param_grid = {
+        'max_depth': [10],  
+        'min_samples_split': [2]
+    }
+
+    # Perform Grid Search with cross-validation
+    grid_search = GridSearchCV(DecisionTreeClassifier(random_state=42), param_grid, cv=5, scoring='recall')
+    grid_search.fit(train_features, train_labels)
+
+    # Plot hyperparameters vs performance (scores)
+    results = grid_search.cv_results_
+    print(f"Min mean test score: {min(results['mean_test_score'])}")
+    print(f"Max mean test score: {max(results['mean_test_score'])}")
+
+    # See DecisionTreeHyperParamTune from inital run
+    # depth_range = np.arange(1, 11)  
+    # split_range = np.arange(2, 11)
+
+    # scores_matrix = results['mean_test_score'].reshape(len(depth_range), len(split_range))
+
+    # plt.figure(figsize=(8, 6))
+    # plt.imshow(scores_matrix, interpolation='nearest', cmap='viridis')
+    # plt.colorbar(label="Mean Test Score")
+    # plt.xticks(np.arange(len(split_range)), split_range)  # x-axis: min_samples_split
+    # plt.yticks(np.arange(len(depth_range)), depth_range)  # y-axis: max_depth
+    # plt.xlabel('min_samples_split')
+    # plt.ylabel('max_depth')
+    # plt.title('Grid Search Performance: Hyperparameters vs Accuracy')
+    # plt.show()
+
+    # Train the model using the best hyperparameters found
+    best_params = grid_search.best_params_
+    best_score = grid_search.best_score_
+
+    print(f"Best params: {best_params}")
+    print(f"Best recall from GridSearchCV: {best_score * 100:.2f}%")
+
+    best_dt_path = './pickledData/bestDecisionTreeModel.pickle'  # Path to the pickled dataset file.
+
+    # Load data from a pickle file or reading CSV file and pickle it if needed. Should only happen once
+    try:
+        with open(best_dt_path, "rb") as file:
+            best_dt = pickle.load(file)
+    except FileNotFoundError:
+        best_dt = DecisionTreeClassifier(random_state=42, **best_params)
+        best_dt.fit(np.concatenate((train_features, validation_features)), np.concatenate((train_labels, validation_labels)))
+        with open(best_dt_path, "wb") as file:
+            pickle.dump(best_dt, file)  # Save decision tree model to a pickle for future use.
+
+    # Test the model on the test data
+    test_predictions = best_dt.predict(test_features)
+
+    # Generate a classification report
+    print("Classification Report:\n", classification_report(test_labels, test_predictions))
+
+    end_time = time.time()
+
+    # Visualize and interpret the decision tree
+    # plt.figure(figsize=(12, 8))
+    # plot_tree(best_dt, filled=True, feature_names=data_feature_names, class_names=data_target_names, fontsize=10)
+    # plt.title("Decision Tree Visualization")
+    # plt.show()
+
+    print("Completed Decision Tree evaluation")
+    print(f"Execution time: {end_time - start_time:.4f} seconds")
--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import naivebayes as nb
-import decisiontree as dt
+import DecisionTree.decisiontree as dt
 import pandas as pd
 import pickle
 from sklearn import preprocessing
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split, GridSearchCV
+import time
+
 
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report
 
-
 # Load the data TAKES WAY TO LONG FOR 6 million records
 # data = pd.read_csv('./data/onlinefraud.csv')
+start_time = time.time()
 
 data_path = './pickledData/fraudData.pickle'  # Path to the pickled dataset file.
 
@@ -52,87 +54,11 @@ temp_features, temp_labels = SMOTE(random_state=130).fit_resample(train_features
 train_features = temp_features
 train_labels = temp_labels
 
-# # Decision Tree
-# dt = DecisionTreeClassifier(random_state=42)
-# dt.fit(train_features, train_labels)
-
-# TODO Pickle the dt
-dt_path = './pickledData/decisionTreeModel.pickle'  # Path to the pickled dataset file.
-
-# Load data from a pickle file or reading CSV file and pickle it if needed. Should only happen once
-try:
-    with open(dt_path, "rb") as file:
-        dt = pickle.load(file)
-except FileNotFoundError:
-    dt = DecisionTreeClassifier(random_state=42)
-    dt.fit(train_features, train_labels)
-    with open(dt_path, "wb") as file:
-        pickle.dump(dt, file)  # Save decision tree model to a pickle for future use.
-
-param_grid = {
-    'max_depth': np.arange(1, 11),  # Depth of the tree (1 to 10)
-    'min_samples_split': np.arange(2, 11)  # Minimum samples to split (2 to 10)
-}
+dt.run_decision_tree(features, labels, train_features, train_labels, validation_features, validation_labels, test_features, test_labels)
 
 
 
-# Perform Grid Search with cross-validation
-grid_search = GridSearchCV(DecisionTreeClassifier(random_state=42), param_grid, cv=5)
-grid_search.fit(train_features, train_labels)
+end_time = time.time()
 
-# Plot hyperparameters vs performance (scores)
-results = grid_search.cv_results_
-print(f"Min mean test score: {min(results['mean_test_score'])}")
-print(f"Max mean test score: {max(results['mean_test_score'])}")
-
-# depth_range = np.arange(1, 11)  
-# split_range = np.arange(2, 11)
-
-# scores_matrix = results['mean_test_score'].reshape(len(depth_range), len(split_range))
-
-# plt.figure(figsize=(8, 6))
-# plt.imshow(scores_matrix, interpolation='nearest', cmap='viridis')
-# plt.colorbar(label="Mean Test Score")
-# plt.xticks(np.arange(len(split_range)), split_range)  # x-axis: min_samples_split
-# plt.yticks(np.arange(len(depth_range)), depth_range)  # y-axis: max_depth
-# plt.xlabel('min_samples_split')
-# plt.ylabel('max_depth')
-# plt.title('Grid Search Performance: Hyperparameters vs Accuracy')
-# plt.show()
-
-# # Train the model using the best hyperparameters found
-best_params = grid_search.best_params_
-best_score = grid_search.best_score_
-
-print(f"Best params: {best_params}")
-print(f"Best recall from GridSearchCV: {best_score * 100:.2f}%")
-
-# TODO Pickle the dt
-best_dt_path = './pickledData/bestDecisionTreeModel.pickle'  # Path to the pickled dataset file.
-
-# Load data from a pickle file or reading CSV file and pickle it if needed. Should only happen once
-try:
-    with open(best_dt_path, "rb") as file:
-        best_dt = pickle.load(file)
-except FileNotFoundError:
-    best_dt = DecisionTreeClassifier(random_state=42, **best_params)
-    best_dt.fit(np.concatenate((train_features, validation_features)), np.concatenate((train_labels, validation_labels)))
-    with open(best_dt_path, "wb") as file:
-        pickle.dump(dt, file)  # Save decision tree model to a pickle for future use.
-
-# best_dt = DecisionTreeClassifier(random_state=42, **best_params)
-# best_dt.fit(np.concatenate((train_features, validation_features)), np.concatenate((train_labels, validation_labels)))
-
-# # Test the model on the test data
-# test_predictions = best_dt.predict(test_features)
-
-# # Generate a classification report
-# print("Classification Report:\n", classification_report(test_labels, test_predictions))
-
-# # Visualize and interpret the decision tree
-# plt.figure(figsize=(12, 8))
-# plot_tree(best_dt, filled=True, feature_names=data.feature_names, class_names=data.target_names, fontsize=10)
-# plt.title("Decision Tree Visualization")
-# plt.show()
-
-print("Done")
+print("All algorithms completed.")
+print(f"Execution time of all algorithms: {end_time - start_time:.4f} seconds")
